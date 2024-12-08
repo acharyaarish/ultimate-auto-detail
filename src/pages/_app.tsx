e3d9308c3d494a37bcdb27ccb9e68b16a7cb6ee6
Arish Acharya <acharyaarish@gmail.com>
2024-12-08 12:01:51 +1100

    Updated Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,23 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        {/* Primary favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* Additional favicons for different devices */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        {/* Android Chrome icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
